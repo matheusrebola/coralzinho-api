418bc5ef86f983c47698df4a70867f56d4749970
Matheus Cavalcanti Rebola <m.cavalcantir@outlook.com>
2023-10-29 13:18:56 -0300

    feat: configuração parcial dos endpoints

---
+++ src/main/java/adv/coral/config/ModelMapperConfig.java
@@ -0,0 +1,2 @@
+package adv.coral.config;public class ModelMapperConfig {
+}
--- src/main/java/adv/coral/controller/AlunoController.java
+++ src/main/java/adv/coral/controller/AlunoController.java
@@ -1,2 +1,52 @@
-package adv.coral.controller;public class AlunoController {
+package adv.coral.controller;
+
+
+import adv.coral.dtos.AlunoDTO;
+import adv.coral.model.Aluno;
+import adv.coral.service.AlunoService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.stream.Collectors;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+@RequiredArgsConstructor
+public class AlunoController {
+    private final AlunoService alunoService;
+    private final ModelMapper modelMapper;
+
+    @GetMapping
+    public ResponseEntity<List<AlunoDTO>> getAll() {
+        List<AlunoDTO> result =
+                alunoService.getAll()
+                        .stream()
+                        .map(this::map)
+                        .collect(Collectors.toList());
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<AlunoDTO> findById(@PathVariable long id) {
+        if (!alunoService.exists(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        AlunoDTO dto = this.map(alunoService.findById(id));
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    private AlunoDTO map(Aluno aluno) {
+        AlunoDTO dto = modelMapper.map(aluno, AlunoDTO.class);
+        return dto;
+    }
 }
