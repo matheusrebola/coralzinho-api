cc7cf59836b8df180b7523f890a0d0b121265d11
Matheus Cavalcanti Rebola <m.cavalcantir@outlook.com>
2023-10-29 12:19:05 -0300

    first commit

---
+++ src/main/java/adv/coral/QrCodeCoralzinhoApplication.java
@@ -0,0 +1,13 @@
+package adv.coral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeCoralzinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeCoralzinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adv/coral/QrCodeCoralzinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package adv.coral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeCoralzinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
