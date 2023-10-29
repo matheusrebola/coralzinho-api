2b6e2524644b22409220f0673e48a5aa155cbe34
Matheus Cavalcanti Rebola <m.cavalcantir@outlook.com>
2023-10-29 12:56:47 -0300

    feat: configuração das services para busca por ID, verificação de existência e retorno de lista, esse último, trazendo todos do banco de dados

--- src/main/java/adv/coral/service/AlunoService.java
+++ src/main/java/adv/coral/service/AlunoService.java
@@ -1,2 +1,26 @@
-package adv.coral.service;public class AlunoService {
+package adv.coral.service;
+
+import adv.coral.model.Aluno;
+import adv.coral.repository.AlunoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AlunoService {
+    private final AlunoRepository alunoRepository;
+
+    public List<Aluno> getAll() {
+        return alunoRepository.findAll();
+    }
+
+    public boolean exists(long id) {
+        return alunoRepository.existsById(id);
+    }
+
+    public Aluno findById(long id) {
+        return alunoRepository.findById(id).orElse(null);
+    }
 }
